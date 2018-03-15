7bc71e0412fd97835e8356bb846ad8ca0091218d
Charlene Orr <charlorrnot@gmail.com>
2018-03-15 17:06:26 -0400

    Words placed, but still overlap

--- WordSearch.java
+++ WordSearch.java
@@ -50,7 +50,102 @@ public class WordSearch
     }
     public void placeWords()
     {
+        Random rand = new Random();
+        for (int i = 0; i < words.length; i++)
+        {
+            String wor = words[i];
+            int a = rand.nextInt(8); // change back to 8
+            int len = wor.length();
+//            a = 7; // for testing
+            switch (a)
+            {
+                case 0: // right
+                        int ro = rand.nextInt(row); // random row
+                        int col = rand.nextInt(column - len + 1);
+                        for (int j = 0; j < len; j++)
+                        {
+                            grid[ro][col + j] = wor.charAt(j);
+                        }
+                        break;
+                case 1: // left
+                        wor = reverse(wor);
+                        ro = rand.nextInt(row); // random row
+                        col = rand.nextInt(column - len + 1);
+                        for (int j = 0; j < len; j++)
+                        {
+                            grid[ro][col + j] = wor.charAt(j);
+                        }
+                        break;
+                case 2: // down
+                        ro = rand.nextInt(row - len + 1); // random row
+                        col = rand.nextInt(column);
+                        for (int j = 0; j < len; j++)
+                        {
+                            grid[ro + j][col] = wor.charAt(j);
+                        }
+                        break;
+                case 3: // up
+                        wor = reverse(wor);
+                        ro = rand.nextInt(row - len + 1); // random row
+                        col = rand.nextInt(column);
+                        for (int j = 0; j < len; j++)
+                        {
+                            grid[ro + j][col] = wor.charAt(j);
+                        }
+                        break;
+                case 4: // down right
+                        ro = rand.nextInt(row - len + 1);
+                        col = rand.nextInt(column - len + 1);
+                        for (int j = 0; j  < len; j++)
+                        {
+                            grid[ro + j][col + j] = wor.charAt(j);
+                        }
+                        break;
+                case 5: // up left
+                        wor = reverse(wor);
+                        ro = rand.nextInt(row - len + 1);
+                        col = rand.nextInt(column - len + 1);
+                        for (int j = 0; j < len; j++)
+                        {
+                            grid[ro + j][col + j] = wor.charAt(j);
+                        }
+                        break;
+                case 6: // up right
+                        ro = rand.nextInt(row - len + 1) + len - 1;
+                        col = rand.nextInt(column - len + 1);
+                        for (int j = 0; j < len; j++)
+                        {
+                            grid[ro - j][col + j] = wor.charAt(j);
+                        }
+                        break;
+                case 7: // down left
+                        wor = reverse(wor);
+                        ro = rand.nextInt(row - len + 1) + len - 1;
+                        col = rand.nextInt(column - len + 1);
+                        for (int j = 0; j < len; j++)
+                        {
+                            grid[ro - j][col + j] = wor.charAt(j);
+                        }
+                        break;
+            }
+        }
 
+    }
+    public boolean checkOverlap(String word, char[][] grid, int rstart, int cstart) // method to check whether overlap is okay
+    {
+        return false;
+    }
+    public String reverse(String forward) // reverses words for use in cases 1, 3, 5, 6, 7
+    {
+        int len = forward.length();
+        char[] back = new char[len];
+        String backward = "";
+        for (int i = 0; i < len; i++)
+        {
+            back[i] = forward.charAt(len - 1 - i);
+            backward += back[i];
+        }
+        return backward;
     }
     public void fillRandom()
     {
@@ -59,16 +154,37 @@ public class WordSearch
         {
             for (int j = 0; j < column; j++)
             {
-                if (grid[i][j] == '*')
-                {
-                    int a = rand.nextInt(26) + 65;
-                    char r = (char)a;
-                    grid[i][j] = r;
-                }
+//                if (grid[i][j] == '*')
+//                {
+//                    int a = rand.nextInt(26) + 65;
+//                    char r = (char)a;
+//                    grid[i][j] = r;
+//                }
                 System.out.printf("%c ", grid[i][j]);
             }
             System.out.printf("\n");
         }
+    }
+    public String[] copyArray(String[] array, int newlen)
+    {
+        String[] newarray = new String[newlen];
+        for (int i = 0; i < newlen; i++)
+        {
+            newarray[i] = array[i];
+        }
+        return newarray;
+    }
+    public char[][] copyArray(char[][] array, int r, int c) // change this because it's wrong
+    {
+        char[][] newarray = new char[r][c];
+        for (int i = 0; i < r; i++)
+        {
+            for (int j = 0; j < c; j++)
+            {
+                newarray[i][j] = array[i][j];
+            }
+        }
+        return newarray;
     }
     public static void main(String[] args)
     {
@@ -81,6 +197,7 @@ public class WordSearch
         title = s.nextLine();
 
         System.out.printf("Enter a maximum of %d words: \n", max); // Change maximum based on size probs
+        int count = 0; // counts true length of filled words array
         for(int i = 0; i < max; i++)
         {
             String next = s.nextLine();
@@ -88,10 +205,12 @@ public class WordSearch
             {
                 break;
             }
-            words[i] = next.replaceAll("\\s+","");
+            words[i] = next.toUpperCase().replaceAll("\\s+","");
+            count++;
         }
-
         WordSearch puzzle = new WordSearch(title, words);
+        puzzle.words = puzzle.copyArray(puzzle.words, count);
+        System.out.println(puzzle.words.length);
         puzzle.createPuzzle(); // places title and initializes grid w/ *'s
         puzzle.placeWords();
         puzzle.fillRandom(); // fills remaining stars and prints puzzle
